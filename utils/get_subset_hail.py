e4a7e082c28745bd434868e6f33171d947a7672f
nikbaya <nikolasbaya@gmail.com>
2021-08-10 13:53:39 +0100

    test version of hail script

--- utils/get_subset_hail.py
+++ utils/get_subset_hail.py
@@ -8,8 +8,67 @@ Created: 2021/08/10
 
 import argparse
 import hail as hl
+import numpy as np
+
+def read_input(input_type, input_path):
+    assert input_type in {"mt"}
+    if input_type == "mt":
+        mt = hl.read_matrix_table(
+            path = input_path
+        )
+    elif input_type == "vcf":
+        pass
+        # mt = hl.import_vcf(
+        #     path = input_path, 
+        #     force_bgz = True
+        # )
+    elif input_type == "plink":
+        pass
+        # mt = hl.import_plink(
+        #     **{suffix: f'{input_path}.{suffix}' for suffix in {"bed", "bim", "fam"}}
+        # )
+    return mt
+
+def get_subset(mt, num_samples):
+    indices = np.random.choice(
+        a = mt.count_cols(),
+        size = num_samples,
+        replace = False
+    )
+    indices.sort() # not important, but will keep samples in the same order
+    mt = mt.choose_cols(indices.tolist())
+    return mt
+
+def write_output(mt, output_type, output_path):
+    assert output_type in {"mt", "plink"}
+    if output_type == "mt":
+        assert ".mt" in output_path
+        mt.write(output_path)
+    elif output_type == "plink":
+        assert "." not in output_path.split("/")[-1]
+        mt.export_plink(
+            dataset = mt,
+            output = output_path
+        )
 
 def main(args):
+    hl.init(log = "logs/get_subset_hail.log")
+    
+    mt = read_input(
+        input_type = args.input_type,
+        input_path = args.input_path
+    )
+    
+    mt = get_subset(
+        mt = mt,
+        num_samples = args.num_samples
+    )
+    
+    write_output(
+        mt = mt, 
+        output_type = args.output_type, 
+        output_path = args.output_path
+    )
     
 
 if __name__=='__main__':
