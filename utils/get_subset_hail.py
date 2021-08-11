d2b91d2f87f5135f3c9f923f87599ae4daf6e0d1
nikbaya <nikolasbaya@gmail.com>
2021-08-11 18:15:04 +0100

    Test version of hail script with submission script

--- utils/get_subset_hail.py
+++ utils/get_subset_hail.py
@@ -10,16 +10,17 @@ import argparse
 import hail as hl
 import numpy as np
 
+
 def read_input(input_type, input_path):
     assert input_type in {"mt"}
     if input_type == "mt":
         mt = hl.read_matrix_table(
-            path = input_path
+            path=input_path
         )
     elif input_type == "vcf":
         pass
         # mt = hl.import_vcf(
-        #     path = input_path, 
+        #     path = input_path,
         #     force_bgz = True
         # )
     elif input_type == "plink":
@@ -29,15 +30,26 @@ def read_input(input_type, input_path):
         # )
     return mt
 
+
+def filter_to_ancestry(mt, ancestry="eur"):
+    assert ancestry in {"all", "eur"}
+    if ancestry == "eur":
+        ht = hl.import_table(
+            "/well/lindgren/UKBIOBANK/laura/k_means_clustering_pcs/ukbb_genetically_european_k4_4PCs_self_rep_Nov2020.txt", key="eid")
+        mt = mt.filter_cols(ht[mt.s].genetically_european == "1")
+    return mt
+
+
 def get_subset(mt, num_samples):
     indices = np.random.choice(
-        a = mt.count_cols(),
-        size = num_samples,
-        replace = False
+        a=mt.count_cols(),
+        size=num_samples,
+        replace=False
     )
-    indices.sort() # not important, but will keep samples in the same order
+    indices.sort()  # not important, but will keep samples in the same order
     mt = mt.choose_cols(indices.tolist())
     return mt
+
 
 def write_output(mt, output_type, output_path):
     assert output_type in {"mt", "plink"}
@@ -47,38 +59,48 @@ def write_output(mt, output_type, output_path):
     elif output_type == "plink":
         assert "." not in output_path.split("/")[-1]
         mt.export_plink(
-            dataset = mt,
-            output = output_path
+            dataset=mt,
+            output=output_path
         )
 
-def main(args):
-    hl.init(log = "logs/get_subset_hail.log")
-    
-    mt = read_input(
-        input_type = args.input_type,
-        input_path = args.input_path
-    )
-    
-    mt = get_subset(
-        mt = mt,
-        num_samples = args.num_samples
-    )
-    
-    write_output(
-        mt = mt, 
-        output_type = args.output_type, 
-        output_path = args.output_path
-    )
-    
 
-if __name__=='__main__':
+def main(args):
+    hl.init(log="logs/get_subset_hail.log")
+
+    mt = read_input(
+        input_type=args.input_type,
+        input_path=args.input_path
+    )
+
+    mt = filter_to_ancestry(
+        mt=mt,
+        ancestry=args.ancestry
+    )
+
+    mt = get_subset(
+        mt=mt,
+        num_samples=args.num_samples
+    )
+
+    write_output(
+        mt=mt,
+        output_type=args.output_type,
+        output_path=args.output_path
+    )
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_path", default=None, help="Path to VCF/MatrixTable/PLINK dataset to subset")
-    parser.add_argument("--input_type", default=None, help="Type of input dataset. Options: 'vcf', 'mt', 'plink'")
+    parser.add_argument("--input_path", default=None,
+                        help="Path to VCF/MatrixTable/PLINK dataset to subset")
+    parser.add_argument("--input_type", default=None,
+                        help="Type of input dataset. Options: 'vcf', 'mt', 'plink'")
     parser.add_argument("--num_samples", help="Number of samples to subset")
-    parser.add_argument("--ancestry", default="EUR", help="Ancestry subset to use. Options: EUR (genetically-confirmed Europeans), ALL (all individuals, no ancestry filter)")
+    parser.add_argument("--ancestry", default="eur",
+                        help="Ancestry subset to use. Options: eur (genetically-confirmed Europeans), all (all individuals, no ancestry filter)")
     parser.add_argument("--out_path", help="Path to output file")
-    parser.add_argument("--out_type", help="Type of output dataset (options: 'vcf', 'mt', 'plink')")
+    parser.add_argument(
+        "--out_type", help="Type of output dataset (options: 'vcf', 'mt', 'plink')")
     args = parser.parse_args()
 
     main(args)
